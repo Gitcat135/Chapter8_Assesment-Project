2f6cbb597f80954497afa2cc37f8e83c01a798ac
Gitcat135 <prince32cat@gmail.com>
2025-10-17 17:08:25 +0300

    Forms Change 2

--- app/forms.py
+++ app/forms.py
@@ -13,6 +13,6 @@ class RegisterForm(FlaskForm):
 class AddproductForm(FlaskForm):
     """Addproduct Form"""
     product_name = StringField('Product Name', validators=[DataRequired(message="Product name is required."),Length(min=2, max=100, message="Product name must be between 2 and 100 characters.")])
-    product_decription = StringField('Product Decription', validators=[DataRequired(message="Product Decription is required."),Length(min=2, max=100, message="Product description must be between 2 and 100 characters.")])
+    product_decription = PasswordField('Product Decription', validators=[DataRequired(message="Product Decription is required."),Length(min=2, max=100, message="Product description must be between 2 and 100 characters.")])
     stock_available = SelectField('Stock Available', choices=[('1', '1'), ('5', '5'), ('12', '12'), ('20', '20')], validators=[DataRequired(message="You must fill the above.")]) 
     submit = SubmitField('Add Product')
